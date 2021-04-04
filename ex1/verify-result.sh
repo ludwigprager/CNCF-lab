3d591c5b4cc59bbaf2d2d7630a432cd4c2923274
Ludwig Prager <ludwig.prager@celp.de>
2021-04-04 19:06:13 +0200

    minor fix in ex1 verfication

--- ex1/verify-result.sh
+++ ex1/verify-result.sh
@@ -9,7 +9,7 @@ error=false
 message=
 
 # test container image
-image=$(kubectl get pod -n$NS nginx -o jsonpath="{$.spec.containers[?(.name=='nginx')].image}")
+image=$(kubectl get pod -n$NS nginx -o jsonpath="{$.spec.containers[?(.name=='nginx')].image}" 2>/dev/null)
 if [[ $image != nginx ]]; then
   error=true
   echo "the container image is wrong"
@@ -17,7 +17,7 @@ fi
 
 
 # test container status
-status=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.status.containerStatuses[?(.name=='nginx')].state.running}")
+status=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.status.containerStatuses[?(.name=='nginx')].state.running}" 2>/dev/null)
 #echo container status: $status
 if [[ -z "$status" ]]; then
   error=true
@@ -26,15 +26,15 @@ fi
 
 
 # test pod status
-status=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.status.phase}")
+status=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.status.phase}" 2>/dev/null)
 if [[ "$status" != "Running" ]] ; then
   error=true
   echo "the pod is not running"
 fi
 
 # test requests
-cpu=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.requests.cpu}")
-memory=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.requests.memory}")
+cpu=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.requests.cpu}" 2>/dev/null)
+memory=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.requests.memory}" 2>/dev/null)
 
 #requests cpu=100m,memory=256Mi and limits cpu=200m,memory=512Mi
 
@@ -42,8 +42,8 @@ memory=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image==
 [[ "$memory" == "256Mi" ]] || error=true
 
 # test limits
-cpu=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.limits.cpu}")
-memory=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.limits.memory}")
+cpu=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.limits.cpu}" 2>/dev/null)
+memory=$(kubectl -n$NS get pod nginx -o=jsonpath="{$.spec.containers[?(@.image=='nginx')].resources.limits.memory}" 2>/dev/null)
 
 [[ "$cpu" == "200m" ]] || error=true
 [[ "$memory" == "512Mi" ]] || error=true
